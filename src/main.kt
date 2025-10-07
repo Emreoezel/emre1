45d976ec03630d69555b21f654f63c23b3378bab
Emreoezel <emre.111@outlook.de>
2025-10-07 15:21:16 +0200

    poem code
    poem code

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,16 @@
 fun main ()
 {
-    print("Hello World")
-    println("Hello Mars")
+    println("Im Code verweilt ein stiller Klang,\n" +
+            "ein Fehler hält dich manchmal lang.\n" +
+            "Doch wenn das Programm dann endlich rennt,\n" +
+            "fühlst du dich wie ein Dirigent.")
+    spaceHolder ()
+    println("Ein „Hello World“ zum ersten Mal,\n" +
+            "der Bildschirm leuchtet, digital.\n" +
+            "Aus Nullen, Einsen wächst die Welt,\n" +
+            "die deine Logik fest zusammenhält.")
+}
+fun spaceHolder ()
+{
+    println("-------------------")
 }
